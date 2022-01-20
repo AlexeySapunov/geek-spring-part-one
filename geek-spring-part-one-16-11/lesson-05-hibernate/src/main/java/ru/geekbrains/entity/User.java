12e62200d49eaf35394bbad824ea9ac4d4f6d16b
Alexey <blacka8607@rambler.ru>
2022-01-20 16:33:21 +0300

    Homework to lesson6

--- geek-spring-part-one-16-11/lesson-05-hibernate/src/main/java/ru/geekbrains/entity/User.java
+++ geek-spring-part-one-16-11/lesson-05-hibernate/src/main/java/ru/geekbrains/entity/User.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,6 +16,9 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Contact> contacts;
 
     public User(Long id, String username, String password) {
         this.id = id;
@@ -47,6 +51,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
     }
 
     @Override
